371acb436434e5e1e29583b281c05e7eca07662e
tinhtt <tinhtrantrung.vn@gmail.com>
2025-04-24 21:52:39 +1200

    Install tailwindcss.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1 className='bg-amber-400 text-6xl'>Hello World</h1>
+  </StrictMode>
+);

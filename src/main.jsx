8ddf6486af65d633bf8def2788957d184e70edd2
tinhtt <tinhtrantrung.vn@gmail.com>
2025-04-24 22:35:04 +1200

    Add shadcn.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Button } from './components/ui/button';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h1 className="bg-amber-400 text-6xl">Hello World</h1>
+    <Button className="cursor-pointer">Click me!</Button>
   </StrictMode>,
 );

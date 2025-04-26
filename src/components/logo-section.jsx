e6ec9311a4608e8b836a6ff53175ed263215c908
tinhtt <tinhtrantrung.vn@gmail.com>
2025-04-26 22:17:21 +1200

    Implement purpose section.

--- src/components/logo-section.jsx
+++ src/components/logo-section.jsx
@@ -1,5 +1,45 @@
+import slack from '@/assets/slack.png';
+import amazon from '@/assets/amazon.png';
+import woocommerce from '@/assets/woocommerce.png';
+import meundies from '@/assets/meundies.png';
+import sitepoint from '@/assets/sitepoint.png';
+
+const logos = [slack, amazon, woocommerce, meundies, sitepoint];
+
 const LogoSection = () => {
-  return <div className="container">Logos</div>;
+  return (
+    <section className="container-center flex flex-col items-start gap-6 overflow-hidden px-0 py-20 sm:flex-row sm:items-center">
+      {/* text */}
+      <div className="z-10 w-xs shrink-0 border-l-4 border-blue-400 bg-white px-6 py-2 text-xl font-semibold text-gray-600 sm:text-base">
+        Proud partner at <br /> Hubspot & Segment
+      </div>
+
+      {/* <div className="z-10 mb-8 w-[300px] shrink-0 border-l-4 border-blue-500 bg-white px-8 py-2 text-xl font-semibold text-gray-600 sm:mb-0 sm:text-left sm:text-base">
+        Proud partner at <br /> Hubspot & Segment
+      </div> */}
+
+      {/* logos */}
+      <div className="animate-marquee flex gap-12">
+        {logos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo}
+            alt={`Company Logo ${index + 1}`}
+            className="h-8 w-36 object-contain opacity-75 grayscale transition-all hover:opacity-100 hover:grayscale-0"
+          />
+        ))}
+        {/* Duplicate logos for seamless loop */}
+        {logos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo}
+            alt={`Company Logo ${index + 1}`}
+            className="h-8 w-36 object-contain opacity-75 grayscale transition-all hover:opacity-100 hover:grayscale-0"
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default LogoSection;
--- src/components/purpose-section.jsx
+++ src/components/purpose-section.jsx
@@ -1,5 +1,38 @@
 const PurposeSection = () => {
-  return <div className="container">Purpose</div>;
+  return (
+    <section className="bg-gray-50 px-4 py-16 md:px-8">
+      <div className="mx-auto grid max-w-6xl gap-8 md:grid-cols-3">
+        <div className="flex flex-col gap-2">
+          <p className="text-sm text-purple-600 uppercase">Achieve more</p>
+          <h2 className="text-3xl font-bold md:w-4/5 md:text-4xl">
+            Purpose of a convoy is to keep your team
+          </h2>
+        </div>
+
+        <div className="flex gap-4">
+          <div className="pt-4">🟣</div>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-xl font-semibold">Built for impact</h3>
+            <p className="text-gray-600">
+              We identify and nurture a truly diverse team of designers,
+              developers and marketers
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-4">
+          <div className="pt-4">🔴</div>
+          <div className="flex flex-col gap-2">
+            <h3 className="text-xl font-semibold">In sync with you</h3>
+            <p className="text-gray-600">
+              We work the way you do by adapting to your workflows and rhythm we
+              aim to blend in for a seamless.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PurposeSection;
